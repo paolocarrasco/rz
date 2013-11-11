c2433f10cfbbf209cffe0d4133d13fc027cd7568
Paolo Carrasco <paolo.carrasco@gmail.com>
2013-11-11 01:09:21 -0300

    considering a backbone collection for the recipes container

--- webapp/js/app/recipes-container.js
+++ webapp/js/app/recipes-container.js
@@ -1,17 +1,25 @@
 !(function(module) {
-    'use strict';
-    var RecipesContainer = function(recipesRepository) {
-        var recipes = [],
-            repository = recipesRepository;
-        this.fetch = function (callback) {
-            repository.list(function(fetchedRecipes) {
-                recipes = fetchedRecipes;
-                callback();
-            });
-        };
-        this.__defineGetter__("recipes", function(){
-            return recipes;
-        });
-    };
-    module.RecipesContainer = RecipesContainer;
+  'use strict';
+  var _repository, _recipes;
+  var RecipesContainer = module.Backbone.Collection.extend({
+    model: function() {
+      
+    },
+    initialize: function(models, options) {
+      _recipes = models || [];
+      _repository = options.repository;
+    },
+
+    // temporary override
+    fetch : function (callback) {
+      _repository.list(function(fetchedRecipes) {
+        _recipes = fetchedRecipes;
+        callback();
+      });
+    },
+    recipes: function(){
+      return _recipes;
+    }
+  });
+  module.RecipesContainer = RecipesContainer;
 })(this);
--- webapp/test/specs/recipes-container.js
+++ webapp/test/specs/recipes-container.js
@@ -1,47 +1,46 @@
 /* global RecipesContainer: true */
 describe('RecipesContainer', function() {
-    "use strict";
+  "use strict";
     
-    var recipesContainer, recipesRepository;
-    
-    beforeEach(function () {
-        recipesRepository = {};
-        recipesRepository.list = function(callback) {
-            var recipes = [
-                {
-                    name: "arroz con pollo",
-                    ingredients: [{
-                        name: "arroz"
-                    }],
-                    description: "comida criolla de arroz y pollo",
-                },
-                {
-                    name: "aji de gallina",
-                    ingredients: [{
-                        name: "aji",
-                        quantity: 2
-                    }],
-                    description: "comida criolla de arroz y pollo",
-                }
-            ];
-            callback(recipes);
-        };
-        recipesContainer = new RecipesContainer(recipesRepository);
+  var recipesContainer, recipesRepository;
+  
+  beforeEach(function () {
+    recipesRepository = {};
+    recipesRepository.list = function(callback) {
+      var recipes = [
+        {
+          name: "arroz con pollo",
+          ingredients: [{
+            name: "arroz"
+          }],
+          description: "comida criolla de arroz y pollo",
+        },
+        {
+          name: "aji de gallina",
+          ingredients: [{
+            name: "aji",
+            quantity: 2
+          }],
+          description: "comida criolla de arroz y pollo",
+        }
+      ];
+      callback(recipes);
+    };
+    recipesContainer = new RecipesContainer([], {
+      repository: recipesRepository
     });
-    
-    it('should start with zero recipes', function () {
-        recipesContainer.recipes.should.have.length(0);
-    });
-    
-    describe('#fetch(callback)', function(){
-        
-        it('should load the given recipes from the repository', function (done) {
-            recipesContainer.fetch(function() {
-                recipesContainer.recipes.should.have.length(2);
-                done();
-            });
-        });
-        
-    });
-        
+  });
+  
+  it('should start with zero recipes', function () {
+    recipesContainer.recipes().should.have.length(0);
+  });
+  
+  describe('#fetch(callback)', function(){
+    it('should load the given recipes from the repository', function (done) {
+      recipesContainer.fetch(function() {
+        recipesContainer.recipes().should.have.length(2);
+        done();
+      });
+    }); 
+  }); 
 });

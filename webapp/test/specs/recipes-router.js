448237037abdf47a7964a555c1b0b2d82c4007d9
Paolo Carrasco <paolo.carrasco@gmail.com>
2013-11-20 02:23:03 -0300

    bf #10: routing the recipe list

--- webapp/test/specs/recipes-router.js
+++ webapp/test/specs/recipes-router.js
@@ -1,48 +1,33 @@
-/* globals RecipesRouter: true */
+/* globals RecipesRouter, Backbone */
 describe('RecipesRouter', function() {
-    var recipesRouter, eventEmitter, pageLocation;
+    var recipesRouter, recipesView;
+
     beforeEach(function() {
-        eventEmitter = (function() {
-            var listeners = {};
-            return {
-                dispatchEvent: function(event) {
-                    listeners[event.type]();
-                },
-                addEventListener: function(eventName, listener) {
-                    listeners[eventName] = listener;
-                }
-            };
-        })();
-        pageLocation = { pathname: '/turron-dona-pepa' };
-        recipesRouter = new RecipesRouter(eventEmitter, pageLocation);
+        var _callback;
+        recipesView = {
+            render: function() {
+                _callback && _callback();
+            },
+            setCallbackOnRender: function(callback) {
+                _callback = callback;
+            }
+        };
+        recipesRouter = new RecipesRouter({
+            view: recipesView
+        });
     });
-    
-    describe('#onSelected(handler)', function() {
-        beforeEach(function() {
-            
+
+    afterEach(function() {
+        Backbone.history.stop();
+    });
+
+    describe('#navigate(root)', function() {
+
+        it('should render the recipesView if the url is the root', function(done) {
+            recipesView.setCallbackOnRender(done);
+            recipesRouter.navigate('');
+            Backbone.history.start({root: "/test", pushState: false});
         });
-        
-        it('should call the registered handler when a recipe is selected select ', function(done) {
-            recipesRouter.onSelected(function() {
-                done();
-            });
-            recipesRouter.select();
-        });
-        
-        it('should send the label to the handler', function(done) {
-            recipesRouter.onSelected(function(label) {
-                label.should.match(/arroz-chaufa/);
-                done();
-            });
-            recipesRouter.select('arroz-chaufa');
-        });
-        
-        it('should call the registered handler when the popstate event is dispatched', function(done) {
-            recipesRouter.onSelected(function(label) {
-                label.should.match(/turron-dona-pepa/);
-                done();
-            });
-            eventEmitter.dispatchEvent({type: 'popstate'});
-        });
+
     });
 });

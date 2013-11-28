7c89c3c8cb5cc34b006c5ea5f0d5acad038c8bf1
Paolo Carrasco <paolo.carrasco@gmail.com>
2013-11-28 00:21:48 -0300

    #18 [@paolocarrasco] added blanket dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-/*global module:false*/
+/*global module, require */
 module.exports = function(grunt) {
 
   // Project configuration.
@@ -62,15 +62,28 @@ module.exports = function(grunt) {
           'reporter': 'spec'
         },
         all: 'webapp/test/**/*.html'
+    },
+    blanket_mocha : {
+        all : ['webapp/test/index.html'],
+        options : {
+            log : true,
+            reporter : 'mocha-unfunk-reporter'
+        }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-mocha-phantomjs');
+  require('mocha-unfunk-reporter').option({
+      "reportPending" : true
+  });
+
+  // loading all the dependency tasks
+  for (var key in grunt.file.readJSON("package.json").devDependencies) {
+      if (key !== "grunt" && key.indexOf("grunt") === 0) {
+          grunt.loadNpmTasks(key);
+      }
+  }
 
   grunt.registerTask('bower_install', function() {
-    /* global require */
     var bower = require('bower');
     var done = this.async();
 
@@ -80,6 +93,7 @@ module.exports = function(grunt) {
         done();
       });
   });
+  grunt.registerTask('coverage', ['blanket_mocha']);
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'bower_install', 'mocha_phantomjs']);

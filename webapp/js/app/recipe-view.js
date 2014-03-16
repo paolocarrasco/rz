3d07a7cd2ce507776779cb5442c728a77d48c1d9
Paolo Carrasco <paolo.carrasco@gmail.com>
2014-03-16 20:24:46 -0300

    #0 [@paolocarrasc] jshint length validation is back for recipe-view.js

--- webapp/js/app/recipe-view.js
+++ webapp/js/app/recipe-view.js
@@ -6,7 +6,7 @@
 
     /* jshint -W101 */
     template: '<div class="ingredients"><ul>{{#ingredients}}<li>{{name}}</li>{{/ingredients}}</ul></div>',
-    /* jshint -W101 */
+    /* jshint +W101 */
 
     render: function() {
       this.el.innerHTML = Mustache.render(this.template, this.model.attributes);

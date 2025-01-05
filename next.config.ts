d8eef4f6929b0e2af33336b3b30d72db3f7afda5
Paolo Carrasco <paolo.carrasco@gmail.com>
2025-01-04 23:39:55 -0500

    #10 chore: NextJs static side considers a base path for assets without image optimization for dev

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === 'production';
+const basePath = isProduction ? '/rz' : '';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/rz',
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: basePath,
+  basePath,
 };
 
 export default nextConfig;

982c2a45571df1a0143df251a5eb04e19005780f
Paolo Carrasco <paolo.carrasco@gmail.com>
2025-01-08 13:35:43 -0500

    #10 chore: Github workflow verifies the coverage of the application

--- jest.config.ts
+++ jest.config.ts
@@ -44,7 +44,14 @@ const config: Config = {
   // ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {
+    "global": {
+      "branches": 90,
+      "functions": 90,
+      "lines": 90,
+      "statements": 90
+    }
+  },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
